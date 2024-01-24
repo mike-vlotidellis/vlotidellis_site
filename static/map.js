7b8fc35acd36eec7130cebabd3af9a2b0283d77c
mike-vlotidellis <m.vlotidellis@gmail.com>
2024-01-24 14:11:21 +0200

    1

--- static/map.js
+++
@@ -1,6 +0,0 @@
-var map = new L.Map('map', {zoom: 15, center: new L.latLng(38.245277, 21.737885) });
-  // Used to load and display tile layers on the map
-  // Most tile servers require attribution, which you can set under `Layer`
-  map.addLayer(new L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}));
